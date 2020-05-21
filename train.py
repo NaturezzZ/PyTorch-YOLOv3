67742c2405f25670b538f25b0ff31a956294503a
NaturezzZ <naturezzz@outlook.com>
2020-05-21 09:02:53 +0000

    little change

--- train.py
+++ train.py
@@ -177,4 +177,4 @@ if __name__ == "__main__":
             print(f"---- mAP {AP.mean()}")
 
         if epoch % opt.checkpoint_interval == 0:
-            torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch+51)
+            torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % (epoch+51))
